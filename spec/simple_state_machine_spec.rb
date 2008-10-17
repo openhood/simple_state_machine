fb485baf1c813f83aa92194039149a67f6047034
openhood <joseph@openhood.com>
2008-10-17 18:51:17 +0200

    Add magic methods to test current state and matching specs.

--- spec/simple_state_machine_spec.rb
+++ spec/simple_state_machine_spec.rb
@@ -82,4 +82,51 @@ describe Chicken do
     end
 
   end
+
+  describe "magic methods to test current state" do
+
+    it "should handle the user_state_pending? method" do
+      c = Chicken.new
+      lambda {c.user_state_pending?}.should_not raise_error NoMethodError
+    end
+
+    it "should evaluate user_state_pending? to true initially" do
+      c = Chicken.new
+      c.user_state_pending?.should === true
+    end
+
+    it "should evaluate user_state_pending? to false after activation" do
+      c = Chicken.new
+      c.user_activate!
+      c.user_state_pending?.should === false
+    end
+
+    it "should evaluate user_state_active? to false initially" do
+      c = Chicken.new
+      c.user_state_active?.should === false
+    end
+
+    it "should evaluate user_state_active? to true after activation" do
+      c = Chicken.new
+      c.user_activate!
+      c.user_state_active?.should === true
+    end
+
+    it "should not catch user_state_invalid?" do
+      c = Chicken.new
+      lambda {c.user_state_invalid?}.should raise_error NoMethodError
+    end
+
+    it "should handle validation_state_waiting?" do
+      c = Chicken.new
+      lambda {c.validation_state_waiting?}.should_not raise_error NoMethodError
+    end
+
+    it "should evaluate validation_state_waiting? to true initially" do
+      c = Chicken.new
+      c.validation_state_waiting?.should === true
+    end
+
+  end
+
 end
